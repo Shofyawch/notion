92bba14aa5e1f7ccf5680a6a3ef74ed162a9bd17
Shofyawch <ashofy20@gmail.com>
2025-11-30 21:56:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,6 @@
 <?php
 $koneksi = mysqli_connect("localhost", "root", "", "multi_user");
 
-<<<<<<< Updated upstream
 //Check Connection
 if(mysqli_connect_errno()){
     echo "Koneksi database gagal : " . mysqli_connect_error();
